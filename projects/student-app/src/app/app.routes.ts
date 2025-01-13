8bb96f0b6fa9c520b72644d25e4f4292d5fb482c
ongzhixian <zhixian@hotmail.com>
2025-01-14 00:05:43 +0800

    add routes for student module

--- projects/student-app/src/app/app.routes.ts
+++ projects/student-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: AppComponent }, 
+    { path:'dashboard', component: DashboardComponent },
+    { path:'lazy-dashboard' , loadComponent: () => import('../app/pages/dashboard/dashboard.component').then(c => c.DashboardComponent) }
+];
