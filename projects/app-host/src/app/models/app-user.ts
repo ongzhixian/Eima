4eafb41b9af7162c8654fa8a8f35002e7b41cdf0
ongzhixian <zhixian@hotmail.com>
2025-01-14 00:25:22 +0800

    add app-user

---
+++ projects/app-host/src/app/models/app-user.ts
@@ -0,0 +1,6 @@
+export interface AppUser {
+    
+    username: string;
+    // email: string;
+    // id: number;
+}
--- projects/app-host/src/app/services/authentication.service.ts
+++ projects/app-host/src/app/services/authentication.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { tap, delay } from 'rxjs/operators';
+import { AppUser } from '../models/app-user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+    isUserLoggedIn: boolean = false;
+
+    login(userName: string, password: string): Observable<AppUser> {
+        console.log(userName);
+        console.log(password);
+        this.isUserLoggedIn = userName == 'admin' && password == 'admin';
+        localStorage.setItem('isUserLoggedIn', this.isUserLoggedIn ? "true" : "false");
+
+        // return this.http.post<Auth>(`${this.apiURLUsers}/login`, { email, password })
+        //     .pipe(
+        //         tap(user => {
+        //         if (user.token) {
+        //             this.setUserSession(user);
+        //         }
+        //         })
+        //     );
+
+        return of(<AppUser>{ username: "asd" }).pipe(
+            delay(1000),
+            tap(val => {
+                console.log("Is User Authentication is successful: " + val);
+            })
+        );
+    }
+
+    logout(): void {
+        this.isUserLoggedIn = false;
+        localStorage.removeItem('isUserLoggedIn');
+    }
+
+    constructor() { }
+
 }
