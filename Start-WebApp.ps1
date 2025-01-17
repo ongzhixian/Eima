40ddc42a797301aa87f6b5603a24d1593afddce4
ongzhixian <zhixian@hotmail.com>
2025-01-17 22:49:46 +0800

    Web script to start app

---
+++ Start-WebApp.ps1
@@ -0,0 +1,19 @@
+npm run start
+# # BUILD ANGULAR APPLICATION
+
+# Write-Host [BUILD ANGULAR APPLICATION]  -ForegroundColor DarkYellow
+
+# npx @angular/cli build app-host --configuration production
+
+
+# # PUBLISH TO AZURE
+
+# Write-Host [PUBLISH TO AZURE]  -ForegroundColor DarkYellow
+
+# Push-Location $PSScriptRoot
+
+# Set-Location .\dist\app-host\browser\
+
+# az webapp up --name eima-webapp --plan eima-app-service-plan --resource-group eima-rg --html
+
+# Pop-Location
