9aa021a06ceac8ad766bdc652ba00e488e4d4b40
ongzhixian <zhixian@hotmail.com>
2025-01-18 09:46:17 +0800

    Toggle navbar based on user authentication status

--- projects/app-host/src/app/components/navbar/navbar.component.ts
+++ projects/app-host/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthenticationService } from '../../services/authentication.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(private authenticationService: AuthenticationService) {
+    
+    this.isAuthenticated$ = this.authenticationService.appUser$.pipe(
+      map(obj => obj.isAuthenticated) 
+    );
+
+  }
 }
