d71b8443fa7405f8aa2d1e5419b588fd8fc2dc06
ongzhixian <zhixian@hotmail.com>
2025-01-18 12:50:24 +0800

    add app-configuration model

--- projects/app-host/src/app/models/app-configuration.ts
+++ projects/app-host/src/app/models/app-configuration.ts
@@ -1,6 +1,33 @@
 export interface AppConfiguration {
-    theme: string; 
+    theme: string;
     language: string;
-
-    apiUrl: string;
+    apiUrls: { [key: string]: string }; // Dictionary type
 }
+
+
+// interface NestedDictionaryModel {
+//     id: number;
+//     name: string;
+//     data: { 
+//       [key1: string]: { 
+//         [key2: string]: any 
+//       } 
+//     };
+//   }
+
+// in json
+// {
+//     "id": 1,
+//     "name": "My Nested Model",
+//     "data": {
+//       "group1": {
+//         "item1": "value1",
+//         "item2": 123,
+//         "item3": true
+//       },
+//       "group2": {
+//         "itemA": "valueA",
+//         "itemB": 456
+//       }
+//     }
+//   }
