8b1672fa06961ff72fea651f831431f92a6ad3b1
ongzhixian <zhixian@hotmail.com>
2025-01-18 14:04:12 +0800

    update selector to distinguish component

--- projects/course-app/src/app/pages/dashboard/dashboard.component.ts
+++ projects/course-app/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'course-app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
