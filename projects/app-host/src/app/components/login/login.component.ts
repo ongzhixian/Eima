0604dd43561839a16b301dad6d4d5815d5d8bc70
ongzhixian <zhixian@hotmail.com>
2025-01-18 09:46:34 +0800

    add login component

---
+++ projects/app-host/src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [ ReactiveFormsModule, NgIf ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+
+    console.log("in onsubmit");
+
+    if (this.loginForm.valid) {
+
+      const { email, password } = this.loginForm.value;
+      
+      this.authService.login(email, password)
+        .subscribe({
+          next: () => {
+            console.log('sucees');
+            this.router.navigate(['/']); 
+          },
+          error: (err) => {
+            // Handle specific errors from your backend 
+            // (e.g., incorrect credentials, server errors)
+            console.error('Login failed:', err); 
+          },
+        });
+    }
+  }
+}
