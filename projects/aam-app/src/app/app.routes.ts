670fc9b0d23d60fa6f99320f0870628fed705d7c
ongzhixian <zhixian@hotmail.com>
2025-01-25 00:28:22 +0800

    EIMA-19 Register User side navigation link

--- projects/aam-app/src/app/app.routes.ts
+++ projects/aam-app/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import {AppComponent} from './app.component';
+import {RegisterUserComponent} from './pages/register-user/register-user.component';
 // import { DashboardComponent } from './pages/dashboard/dashboard.component';
 // import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path:'', component: DashboardComponent },
-    { path:'dashboard', component: DashboardComponent }
+    { path:'dashboard', component: DashboardComponent },
+    { path:'register-user', component: RegisterUserComponent }
     // { path:'', component: AppComponent },
     // { path:'dashboard', component: DashboardComponent },
     // { path:'lazy-dashboard' , loadComponent: () => import('../app/pages/dashboard/dashboard.component').then(c => c.DashboardComponent) }
--- projects/aam-app/src/app/components/side-navbar/side-navbar.component.ts
+++ projects/aam-app/src/app/components/side-navbar/side-navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'aam-side-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './side-navbar.component.html',
   styleUrl: './side-navbar.component.css'
 })
