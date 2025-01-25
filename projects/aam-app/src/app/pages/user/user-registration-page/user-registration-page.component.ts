20667dcddead646945d6e5b143aac77227bdd1b9
ongzhixian <zhixian@hotmail.com>
2025-01-25 14:57:55 +0800

    EIMA-31 Add user registration page

---
+++ projects/aam-app/src/app/pages/user/user-registration-page/user-registration-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aam-user-registration-page',
+  imports: [],
+  templateUrl: './user-registration-page.component.html',
+  styleUrl: './user-registration-page.component.css'
+})
+export class UserRegistrationPageComponent {
+
+}
