523b05338a0f01f4ca0d3b688ddea8640d504499
ongzhixian <zhixian@hotmail.com>
2025-01-25 14:58:26 +0800

    EIMA-28 Add user list page

--- projects/aam-app/src/app/app.routes.ts
+++ projects/aam-app/src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import {RegisterUserComponent} from './pages/register-user/register-user.component';
+
+//import {LoginPageComponent} from '../../../app-host/src/app/pages/login-page/login-page.component';
+import {UserListPageComponent} from './pages/user/user-list-page/user-list-page.component';
+import {UserRegistrationPageComponent} from './pages/user/user-registration-page/user-registration-page.component';
 // import { DashboardComponent } from './pages/dashboard/dashboard.component';
 // import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path:'', component: DashboardComponent },
     { path:'dashboard', component: DashboardComponent },
-    { path:'register-user', component: RegisterUserComponent }
+
+    //{ path:'register-user', component: RegisterUserComponent },
     // { path:'', component: AppComponent },
     // { path:'dashboard', component: DashboardComponent },
     // { path:'lazy-dashboard' , loadComponent: () => import('../app/pages/dashboard/dashboard.component').then(c => c.DashboardComponent) }
+    {
+      path: 'user',
+      children: [
+        { path:'list', component: UserListPageComponent },
+        { path:'register', component: UserRegistrationPageComponent },
+      ]
+    }
 ];
---
+++ projects/aam-app/src/app/pages/user/user-list-page/user-list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aam-user-list-page',
+  imports: [],
+  templateUrl: './user-list-page.component.html',
+  styleUrl: './user-list-page.component.css'
+})
+export class UserListPageComponent {
+
+}
