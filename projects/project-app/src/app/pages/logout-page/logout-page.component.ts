7676ef681f94bb1c4b2a3d82d50b59b6b52a1450
ongzhixian <zhixian@hotmail.com>
2025-02-17 22:16:51 +0800

    EIMA-61 Add logout page

---
+++ projects/project-app/src/app/pages/logout-page/logout-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-app-logout-page',
+  imports: [],
+  templateUrl: './logout-page.component.html',
+  styleUrl: './logout-page.component.css'
+})
+export class LogoutPageComponent {
+
+}
