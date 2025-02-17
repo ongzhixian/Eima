f79dc2a6f28a15f0dd5e6b27f894e9b2eba26a78
ongzhixian <zhixian@hotmail.com>
2025-02-17 22:17:53 +0800

    EIMA-63 Add project list page

---
+++ projects/project-app/src/app/pages/project/project-list-page/project-list-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'project-app-project-list-page',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './project-list-page.component.html',
+  styleUrl: './project-list-page.component.css'
+})
+export class ProjectListPageComponent {
+
+}
